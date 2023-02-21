46e87074ee0bc10f9132ef6643f82c426698065b
Pj Metz <65838556+MetzinAround@users.noreply.github.com>
2023-02-21 13:40:16 -0800

    lets see what works

---
+++ bot.py
@@ -0,0 +1,42 @@
+# This example requires the 'message_content' intent.
+
+import discord
+import config as conf
+import schedule as sched
+import time
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+
+    if message.author == client.user:
+        return 
+
+    if "$bizzy_bone" in message.content.lower():
+        await message.channel.send("I Miss my uncle Charles y'all... https://youtu.be/DUBC47Z7i6A?t=99")
+        return
+
+    if "$crossroads" in message.content.lower():
+        await message.channel.send("What you gonna do?   https://youtu.be/VMYAEHE2GrM?t=52")
+        return
+
+async def bone_message():
+    if date.today().day != 1:
+        return
+    else:    
+        print("It's the first of the Month...")
+        message.channel.send("It's the first of the mooooonth... https://youtu.be/4j_cOsgRY7w?t=9")
+        return
+
+
+sched.every().day.at("11:00").do(bone_message)
+client.run(conf.secret_token)
