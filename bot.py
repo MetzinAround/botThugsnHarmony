3c99354d195fc0a10e9ec1582ce068ce8ed7490a
Pj Metz <65838556+MetzinAround@users.noreply.github.com>
2023-02-23 12:00:06 -0800

    added comments.

--- bot.py
+++ bot.py
@@ -6,22 +6,34 @@ import schedule as sched
 from datetime import date
 
 
+# Intents allow the bot to have the ability to receive and send requests. 
+# https://discordpy.readthedocs.io/en/stable/api.html#discord.Intents
 
 intents = discord.Intents.default()
 intents.message_content = True
 
+# turns on the intents in a keyword `client` that will be used to grab data like
+# user names and to create events, decorators used to declare async events. 
+# https://discordpy.readthedocs.io/en/stable/api.html?highlight=client%20event#discord-api-events
+# https://discordpy.readthedocs.io/en/stable/api.html?highlight=client%20event#discord.Client.event
 client = discord.Client(intents=intents)
 
+
 @client.event
+# on_ready() is called when the conection is made. 
 async def on_ready():
     print(f"We have logged in as {client.user}")
 
 @client.event
+# on_message() is called when a message is sent to the discord server/channel. 
 async def on_message(message):
-
+    # keeps the bot from responding to itself. 
     if message.author == client.user:
         return 
 
+    # turns the message contents to lower case and checks if it contains
+    # the string `$bizzy_bone`. Then, the bot usses `message.channel.send` 
+    # to reply in the channel. 
     if "$bizzy_bone" in message.content.lower():
         await message.channel.send("I Miss my uncle Charles y'all... https://youtu.be/DUBC47Z7i6A?t=99")
         return
@@ -34,16 +46,17 @@ async def on_message(message):
 """
 @client.event
 async def bone_message(message):
-    if date.today().day != 21:
+    if date.today().day != 1:
         return
     else:    
         print("It's the first of the Month...")
         message.channel.send("It's the first of the mooooonth... https://youtu.be/4j_cOsgRY7w?t=9")
         return
 
-# sched.every().day.at("14:17").do(bone_message)
+# sched.every().day.at("10:15").do(bone_message)
 """
 
-
+# turns on the event listeners. Immediately runs on_ready() once connected. 
+# conf.secret_token is from the config file. 
 client.run(conf.secret_token)
 
